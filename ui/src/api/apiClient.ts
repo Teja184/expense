5a06ebbbfcfbeeca92abab754e610770493a479e
Teja184 <gtejavardhanreddy974@gmail.com>
2025-03-10 19:52:39 +0530

    updated api call according to environments

--- ui/src/api/apiClient.ts
+++ ui/src/api/apiClient.ts
@@ -1,7 +1,8 @@
 import axios from "axios";
+const apiUrl = import.meta.env.VITE_API_URL;
 
 const apiClient = axios.create({
-    baseURL: "http://localhost:5000/api",
+    baseURL: apiUrl,
     headers: {
         "Content-Type": "application/json"
     },
